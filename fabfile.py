a958331e0268cc01c78609c3dcf2695e8b8a4027
Dan Rowden <hello@danrowden.com>
2016-03-21 08:40:40 +0400

    Fab file for rsync deployment

---
+++ fabfile.py
@@ -0,0 +1,18 @@
+from fabric.api import *
+from fabric.contrib.project import rsync_project
+ 
+# replace username in the next path. Can't use $HOME from python.
+webfaction_user = 'danr'
+webfaction_app = 'danrowden'
+webfaction_server = 'web398'
+remote_dir = '/home/{}/webapps/{}/'.format(webfaction_user, webfaction_app)
+ 
+def prod():
+    env.hosts = ['{}@{}.webfaction.com'.format(webfaction_user, webfaction_server)]
+ 
+def deploy():
+    rsync_project(
+        remote_dir = remote_dir,
+        local_dir = "",
+        exclude = ("*.DS_Store, README.md, .gitignore, *.sublime-project",),
+    )
